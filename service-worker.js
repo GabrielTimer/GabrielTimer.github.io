d5abe5a36d8c94c505074c2a02a7b5810264ea3d
Timer <g.laveau@yahoo.fr>
2026-03-04 14:41:22 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "workout-timer-v1";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/timer.html",
+"/logo.png",
+"/manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+
+);
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response => response || fetch(event.request))
+
+);
+
+});
