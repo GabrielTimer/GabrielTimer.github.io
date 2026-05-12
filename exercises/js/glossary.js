25cf64f4794a26fbfb9595a815d10a3e7090f4a5
Timer <g.laveau@yahoo.fr>
2026-05-12 10:31:24 +0200

    Add files via upload

--- exercises/js/glossary.js
+++ exercises/js/glossary.js
@@ -1,1 +1,26 @@
+fetch('data/exercises.json')
+  .then(response => response.json())
+  .then(exercises => {
 
+    const container = document.getElementById('exercise-container');
+
+    exercises.forEach(exercise => {
+
+      const card = document.createElement('div');
+      card.className = 'exercise-card';
+
+      card.innerHTML = `
+        <img src="${exercise.image}" alt="${exercise.nom}">
+        
+        <h2>${exercise.nom}</h2>
+
+        <p>${exercise.objectif}</p>
+
+        <button>Lancer</button>
+      `;
+
+      container.appendChild(card);
+
+    });
+
+  });
